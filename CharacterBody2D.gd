ad15c6ff4e492d0f52ce56e83e1fa26721389780
Jorgelino328 <jorgewilliam328@gmail.com>
2023-09-23 20:18:11 -0300

    char collision

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,14 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var state := 0 #Armed is default
+
 var direction
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"): 
-		$Sprites/Armed.hide();
-		$Sprites/Unarmed.show();
 
 func _physics_process(delta):
 	# Add the gravity.
