bfadab55f904dfbfd4a4f308ca4a645aaf42e53a
Jorgelino328 <jorgewilliam328@gmail.com>
2023-09-24 07:56:15 -0300

    beast

---
+++ Characters/Besta/Besta.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+var can_shoot := false
+var id = 0
+var flecha = preload("res://Scenes/Flecha/Flecha.tscn")
+
+func _process(delta):
+	if(can_shoot):
+		var arrow_instance = flecha.instantiate()
+		arrow_instance.global_position = $Marker2D.global_position
+		self.get_tree().get_root().add_child(arrow_instance)
+		can_shoot = false
+
+func _on_timer_timeout():
+	can_shoot = true
---
+++ Scenes/Flecha/Flecha_2.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+@onready var speed = 10
+var direction := Vector2(1,0)
+
+func _ready():
+	gravity_scale = 0
+
+func _physics_process(delta):
+	if direction != Vector2.ZERO:
+		var velocity = direction * speed 
+		global_position += velocity
+	
+func set_direction(direction: Vector2):
+		self.direction = direction
+		
+func _on_body_entered(body):
+		queue_free()
