6bc7d641ec71dd33e3325e58a468478908a97de2
Armando <armandoabreu.filho@hotmail.com>
2023-09-24 00:00:06 -0300

    lv3.5 rework

---
+++ Scenes/Scene_3.5/Scene 3.5.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var previous_cena := load("res://Scenes/Scene_4/Scene_4.tscn")
+
+signal next_level(previous_cena, x);
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_area_2d_body_entered(body):
+	emit_signal("next_level",previous_cena,1)
